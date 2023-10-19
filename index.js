0103d789606b5e905b49908d63744bfae45936b0
Zisan05 <mdzisanislam69@gmail.com>
2023-10-19 07:54:49 -0700

    delete part done

--- index.js
+++ index.js
@@ -76,6 +76,14 @@ async function run() {
     res.send(result)
 } )
 
+
+
+app.delete("/card/:id",async(req,res)=>{
+               const id = req.params.id;
+               const query = {_id: new ObjectId(id)};
+               const result = await userStroge.deleteOne(query);
+               res.send(result);
+          })
        
 
     // Send a ping to confirm a successful connection
